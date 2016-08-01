d22e9c39eccab33188b21441929462a025b446b6
Hawken Rives <hawkrives@gmail.com>
2016-08-02 07:28:23 +0900

    initial (working) thing!

--- referee/__main__.py
+++ referee/__main__.py
@@ -1,17 +1,20 @@
-from argparse import ArgumentParser
+import argparse
+import server
 
-def args():
-    argp = ArgumentParser()
-    subparsers = argp.add_subparsers()
+def arguments():
+    argp = argparse.ArgumentParser()
+    subparsers = argp.add_subparsers(title='subcommands')
 
     serve = subparsers.add_parser('serve')
     serve.add_argument('-a', help='a flag')
+    serve.set_defaults(__command=server.run)
 
     return argp.parse_args()
 
 
 def main():
-    print(args())
+    args = arguments()
+    args.__command()
 
 if __name__ == '__main__':
     main()
